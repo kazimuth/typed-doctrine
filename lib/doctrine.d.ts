0a7d2353f4495fc34d0c22997900f2a8c93bca80
James Gilles <jhgilles@mit.edu>
2016-07-12 21:14:28 -0400

    Add typings, basic tests

--- lib/doctrine.d.ts
+++ lib/doctrine.d.ts
@@ -1,5 +1,324 @@
 declare namespace doctrine {
 
+  /**
+   * Literal '?'.
+   */
+  export interface NullableLiteral {
+    // '?'
+    type: 'NullableLiteral';
+  }
+
+  /**
+   * Literal '*'.
+   */
+  export interface AllLiteral {
+    // '*'
+    type: 'AllLiteral';
+  }
+
+  /**
+   * Literal 'null'.
+   */
+  export interface NullLiteral {
+    type: 'NullLiteral';
+  }
+
+  /**
+   * Literal 'undefined'.
+   */
+  export interface UndefinedLiteral {
+    type: 'UndefinedLiteral';
+  }
+
+  /**
+   * A union of types, expressed as (type1 | type2 | type3)...
+   */
+  export interface UnionType {
+    type: 'UnionType';
+    elements: Array<Type>;
+  }
+
+  /**
+   * An array of types. [1, 2, 'hello'] would be expressed as [Number, Number, String].
+   */
+  export interface ArrayType {
+    type: 'ArrayType';
+    elements: Array<Type>;
+  }
+
+  /**
+   * A type that cannot be null. Expressed as !type or type!.
+   */
+  export interface NonNullableType {
+    type: 'NonNullableType';
+    expression: Type;
+    prefix: boolean;
+  }
+
+  /**
+   * A type that can be null. Expressed as ?type or type?.
+   */
+  export interface NullableType {
+    type: 'NullableType';
+    expression: Type;
+    prefix: boolean;
+  }
+
+  /**
+   * A type name.
+   */
+  export interface NameExpression {
+    type: 'NameExpression';
+    name: string;
+  }
+
+  /**
+   * An application of a generic type, e.g. Array<string>.
+   */
+  export interface TypeApplication {
+    type: 'TypeApplication';
+    expression: Type;
+    applications: Array<Type>;
+  }
+
+  /**
+   * An anonymous record type, expressed as {key1:type1, key2:type2}.
+   */
+  export interface RecordType {
+    type: 'RecordType';
+    fields: Array<FieldType>;
+  }
+
+  /**
+   * A field in a record type. expressed as fieldKey:type.
+   */
+  export interface FieldType {
+    type: 'FieldType';
+    key: string;
+    value?: Type;
+  }
+
+  /**
+   * A function type, expressed as function(arg1, arg2): returntype.
+   */
+  export interface FunctionType {
+    type: 'FunctionType';
+    params: Array<Type>;
+    result?: Type;
+  }
+
+  /**
+   * An optional function parameter, expressed as 'type='.
+   * Only permitted in parameter locations, i.e. in a FunctionType
+   * or a 'param' tag.
+   */
+  export interface OptionalType {
+    type: 'OptionalType';
+    expression: Type;
+  }
+
+  /**
+   * A function parameter name with type, expressed as 'name:type'.
+   * only permitted in parameter locations, i.e. in a functiontype
+   * or a 'param' tag.
+   */
+  export interface ParameterType {
+    type: 'ParameterType';
+    name: string;
+    expression: Type;
+  }
+
+  /**
+   * A function parameter name permitted a variable number of times, expressed as ...type.
+   * only permitted in parameter locations, i.e. in a functiontype
+   * or a 'param' tag.
+   */
+  export interface RestType {
+    type: 'RestType';
+    expression: Type;
+  }
+
+  /**
+   * A function returning 'void'.
+   * Only allowed in the result field on a FunctionTag.
+   */
+  export interface VoidLiteral {
+    // 'void'
+    type: 'VoidLiteral';
+  }
+
+  /**
+   * All possible types.
+   */
+  export type Type = NullableLiteral | AllLiteral | NullLiteral | UndefinedLiteral | UnionType | ArrayType | RecordType | FunctionType | NonNullableType | OptionalType | NullableType | NameExpression | TypeApplication | OptionalType | ParameterType | RestType | VoidLiteral;
+
+  /**
+   * An @-tag.
+   * Note that the semantics of what titles go with what fields is somewhat complex;
+   * see usejsdoc.org and/or doctrine's source code.
+   */
+  export interface Tag {
+    /**
+     * The title of the tag. What goes after the @.
+     */
+    title: string;
+    /**
+     * The description of the tag.
+     */
+    description?: string;
+    /**
+     * If the tag includes a 'namepath', it is included here.
+     * e.g.: for 'param {string} bananas', this will include 'bananas'.
+     */
+    name?: string;
+    /**
+     * The type included in the tag, if any.
+     */
+    type?: Type;
+    /**
+     * Errors encountered during parsing, if 'recoverable' is set.
+     */
+    errors?: Array<string>;
+    /**
+     * The line number, if 'lineNumbers' is set
+     */
+    lineNumber?: number;
+    /**
+     * The caption of an 'example' tag.
+     */
+    caption?: string;
+    /**
+     * The variation index in a 'variation' tag.
+     */
+    variation?: string;
+    /**
+     * The access parameter in an 'access' tag.
+     */
+    access?: string;
+  }
+
+  /**
+   * A comment.
+   */
+  export interface Comment {
+    /**
+     * The description of the comment.
+     * NOTE: doctrine does not understand 'description' tags; you'll have to
+     * look those up yourself.
+     */
+    description: string;
+    /**
+     * The tags in the comment, i.e. things starting with '@'.
+     */
+    tags: Array<Tag>;
+  }
+
+  /**
+   * The library version.
+   */
+  export const version: '1.2.2';
+
+  /**
+   * Parse a doc comment.
+   */
+  export function parse(
+    comment: string,
+    options?: {
+      /**
+       * Set to true to delete the leading / *, any * that begins a line, and
+       * the trailing * / from the source text. Default: false.
+       */
+      unwrap?: boolean,
+      /**
+       * An array of tags to return. When specified, Doctrine returns only tags
+       * in this array. For example, if tags is ['param'], then only param tags will be returned. Default: null.
+       */
+      tags?: Array<string>,
+      /**
+       * Set to true to keep parsing even when syntax errors occur. Default: false.
+       */
+      recoverable?: boolean,
+      /**
+       * Set to true to allow optional parameters to be specified in brackets (@param {string} [foo]). Default: false.
+       */
+      sloppy?: boolean,
+      /**
+       * set to true to add lineNumber to each node, specifying the line on which the node is found in the source. Default: false.
+       */
+      lineNumbers?: boolean
+    }
+  ): Comment;
+
+  // Work around the awkward repetition of functions in different modules
+  type ParseTypeFn = (type: string) => Type;
+  type ParseParamTypeFn = (type: string) => Type;
+
+  /**
+   * Parse a jsdoc type expression.
+   */
+  export const parseType: ParseTypeFn;
+
+  /**
+   * Parse a jsdoc parameter type expression.
+   * Includes anything that can come out of {@link doctrine.parseType}, as well as
+   */
+  export const parseParamType: ParseParamTypeFn;
+
+  type SyntaxType = {
+    NullableLiteral: 'NullableLiteral';
+    AllLiteral: 'AllLiteral';
+    NullLiteral: 'NullLiteral';
+    UndefinedLiteral: 'UndefinedLiteral';
+    VoidLiteral: 'VoidLiteral';
+    UnionType: 'UnionType';
+    ArrayType: 'ArrayType';
+    RecordType: 'RecordType';
+    FieldType: 'FieldType';
+    FunctionType: 'FunctionType';
+    ParameterType: 'ParameterType';
+    RestType: 'RestType';
+    NonNullableType: 'NonNullableType';
+    OptionalType: 'OptionalType';
+    NullableType: 'NullableType';
+    NameExpression: 'NameExpression';
+    TypeApplication: 'TypeApplication'
+  };
+
+  /**
+   * All possible 'type's in the type AST.
+   */
+  export const Syntax: SyntaxType;
+
+  /**
+   * The 'type' namespace, for functions relevant to jsdoc type expressions.
+   */
+  export namespace type {
+    /**
+     * All possible 'type's in the type AST.
+     */
+    export const Syntax: SyntaxType;
+
+    /**
+     * Parse a jsdoc type expression.
+     */
+    export const parseType: ParseTypeFn;
+
+    /**
+     * Parse a jsdoc parameter type expression.
+     * Includes anything that can come out of {@link doctrine.parseType}, as well as
+     */
+    export const parseParamType: ParseParamTypeFn;
+
+    /**
+     * Convert a type AST to a string.
+     */
+    export function stringify(
+      node: Type,
+      options?: {
+        compact?: boolean,
+        topLevel?: boolean
+      }): string;
+  }
 }
 
 export = doctrine;
--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,94 @@
-import test = require('blue-tape');
+import * as d from 'doctrine';
+import * as test from 'blue-tape';
 
-import doctrine = require('doctrine');
+test('parse no options', (t: test.Test) => {
+  let res = d.parse('');
+  t.plan(2)
+  t.equal(res.description, '');
+  t.equal(res.tags.length, 0);
+});
+
+test('parse empty options', (t: test.Test) => {
+  let res = d.parse('', {});
+  t.equal(res.description, '');
+  t.equal(res.tags.length, 0);
+  t.end();
+});
+
+test('parse all options', (t: test.Test) => {
+  let res = d.parse('/* this is a test\n * @param {number} bees the number of bees\n */', {
+    unwrap: true,
+    tags: null,
+    recoverable: true,
+    sloppy: true,
+    lineNumbers: true
+  });
+  t.plan(10);
+  t.equal(res.description, 'this is a test');
+  t.equal(res.tags.length, 1);
+  t.equal(res.tags[0].title, 'param');
+  t.equal(res.tags[0].name, 'bees');
+  t.equal(res.tags[0].description, 'the number of bees');
+  t.equal(res.tags[0].type.type, d.Syntax.NameExpression);
+  let resType = res.tags[0].type as d.NameExpression;
+  t.equal(resType.name, 'number');
+  t.equal(res.tags[0].lineNumber, 1);
+  t.false(res.tags[0].errors);
+  t.false(res.tags[0].caption);
+  t.end();
+});
+
+test('parseType all types', (t: test.Test) => {
+  let res = d.parseType('(undefined|null|*|?|Dog|Dog?|Dog!|Dog[]|Array<Dog>|[Dog,Cat]|function(Bird=):void|{rex:Dog}|function(...Dog))');
+  t.equal(res.type, d.Syntax.UnionType);
+  let u = res as d.UnionType;
+  t.equal(u.elements.length, 13);
+  t.equal(u.elements[0].type, d.Syntax.UndefinedLiteral);
+  t.equal(u.elements[1].type, d.Syntax.NullLiteral);
+  t.equal(u.elements[2].type, d.Syntax.AllLiteral);
+  t.equal(u.elements[3].type, d.Syntax.NullableLiteral);
+  t.equal(u.elements[4].type, d.Syntax.NameExpression);
+  t.equal(u.elements[5].type, d.Syntax.NullableType);
+  t.equal(u.elements[6].type, d.Syntax.NonNullableType);
+  t.equal(u.elements[7].type, d.Syntax.TypeApplication);
+  t.equal(u.elements[8].type, d.Syntax.TypeApplication);
+  t.equal(u.elements[9].type, d.Syntax.ArrayType);
+  t.equal(u.elements[10].type, d.Syntax.FunctionType);
+  t.equal(u.elements[11].type, d.Syntax.RecordType);
+
+  t.equal((u.elements[4] as d.NameExpression).name, 'Dog');
+
+  t.equal((u.elements[5] as d.NullableType).prefix, false);
+  t.equal((u.elements[5] as d.NullableType).expression.type, d.Syntax.NameExpression);
+  t.equal(((u.elements[5] as d.NullableType).expression as d.NameExpression).name, 'Dog');
+
+  t.equal((u.elements[6] as d.NonNullableType).prefix, false);
+  t.equal((u.elements[6] as d.NonNullableType).expression.type, d.Syntax.NameExpression);
+  t.equal(((u.elements[6] as d.NonNullableType).expression as d.NameExpression).name, 'Dog');
+
+  t.equal((u.elements[7] as d.TypeApplication).expression.type, d.Syntax.NameExpression);
+  t.equal(((u.elements[7] as d.TypeApplication).expression as d.NameExpression).name, 'Array');
+  t.equal((u.elements[7] as d.TypeApplication).applications.length, 1);
+  t.equal((u.elements[7] as d.TypeApplication).applications[0].type, d.Syntax.NameExpression);
+  t.equal(((u.elements[7] as d.TypeApplication).applications[0] as d.NameExpression).name, 'Dog');
+
+  t.equal((u.elements[8] as d.TypeApplication).expression.type, d.Syntax.NameExpression);
+  t.equal(((u.elements[8] as d.TypeApplication).expression as d.NameExpression).name, 'Array');
+  t.equal((u.elements[8] as d.TypeApplication).applications.length, 1);
+  t.equal((u.elements[8] as d.TypeApplication).applications[0].type, d.Syntax.NameExpression);
+  t.equal(((u.elements[8] as d.TypeApplication).applications[0] as d.NameExpression).name, 'Dog');
+
+  let a = u.elements[9] as d.ArrayType;
+  t.equal(a.elements.length, 2);
+  t.equal(a.elements[0].type, d.Syntax.NameExpression);
+  t.equal(a.elements[1].type, d.Syntax.NameExpression);
+  t.equal((a.elements[0] as d.NameExpression).name, 'Dog');
+  t.equal((a.elements[1] as d.NameExpression).name, 'Cat');
+
+  let f = u.elements[10] as d.FunctionType;
+  t.equal(f.result.type, d.Syntax.VoidLiteral);
+  t.equal(f.params.length, 1);
+  t.equal(f.params[0].type, d.Syntax.OptionalType);
+  t.equal(f.params[0].type, d.Syntax.OptionalType);
+  t.end();
+});
